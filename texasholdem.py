43e48219aaabc7f515d9b65781a358f6325d75ad
BrownLunch <gburger125@gmail.com>
2024-01-09 22:33:15 +0900

    諸々変更

---
+++ texasholdem.py
@@ -0,0 +1,25 @@
+def game():
+    prefrop()
+    frop()
+    turn()
+    river()
+    showdown()
+
+def betting_round():
+    pass
+
+def prefrop():
+    betting_round()
+
+def frop():
+    betting_round()
+
+def turn():
+    betting_round()
+
+def river():
+    betting_round()
+
+def showdown():
+    # 
+    pass
