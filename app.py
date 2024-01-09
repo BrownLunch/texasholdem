7095f288ef1b5d76c8e092a15ee9a23321b4a5c6
BrownLunch <gburger125@gmail.com>
2024-01-09 20:45:32 +0900

    socketioを追加

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "nkpoker"
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
