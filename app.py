74d00459d76dce1d3ee7a327254950e1f6273686
BrownLunch <gburger125@gmail.com>
2024-01-24 13:59:00 +0900

    mormor

--- app.py
+++ app.py
@@ -4,9 +4,29 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "nkpoker"
 socketio = SocketIO(app)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    html = '''<div id="title-bg">
+                <div id="title-container"> 
+                    <div class="title">
+                        <img id="title-img" src="../static/images/title.png" alt="タイトル">
+                    </div>
+                    <div class="title-nav">
+                        <img id="title-nav-img" src="../static/images/clickanywhere.png" alt="クリックしてください">
+                    </div>   
+                </div>             
+            </div>'''
+    return render_template("index.html", html=html)
+
+@socketio.on("video_request")
+def handle_video_request():
+    html = """<video id=switchingbg controls autoplay>
+    <source src="../videos/switching.mp4" type="video/mp4">
+    </video> 
+"""
+
+
 
 if __name__ == "__main__":
     socketio.run(app, port=50002, debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,17 @@
 let socket = io.connect('http://' + document.domain + ':' + location.port);
 
-$("titlebg").addEventListener("mouseup", switching_menu());
-
-
-
 function switching_menu(){
-    
+    console.log(111)
+        $(".title-nav").fadeOut(1500); //アニメーション変更の可能性あり
+        $(".title").fadeOut(1500); //アニメーション変更の可能性あり
 }
+
+$(window).on("load", function(){
+    $("#title-bg").on("mouseup", switching_menu)
+
+})
+
+
+
+
 
