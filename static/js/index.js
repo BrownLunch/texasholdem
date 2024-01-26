01dd738046da472963824ec68d307aa5a760f3e6
BrownLunch <gburger125@gmail.com>
2024-01-26 23:23:11 +0900

    mataasita

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,8 @@ function switching_menu(){
     $(".title-nav").fadeOut(1000); //アニメーション変更の可能性あり
     $(".title").fadeOut(1000); //アニメーション変更の可能性あり
     $("#bg").removeClass("start")
+    $("#bg").addClass("end")
+    $("#bg").html()
     $("body").off("mouseup", switching_menu);
 }
 
