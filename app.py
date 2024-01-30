a80592f1ca8d568823d723367548f04342d02657
BrownLunch <gburger125@gmail.com>
2024-01-30 12:33:01 +0900

    諸々

--- app.py
+++ app.py
@@ -7,28 +7,16 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    html = """<div id="title-container"> 
-        <div class="title">
-            <img id="title-img" src="../static/images/title.png" alt="タイトル">
-        </div>
-        <div class="title-nav">
-            <img id="title-nav-img" src="../static/images/clickanywhere.png" alt="クリックしてください">
-        </div>   
-    </div>             
-    <video id=switchingbg preload="auto" poster="../static/images/titlebg.png">
-        <source src="../static/switchingbg.mp4" type="video/mp4">
-    </video>
-    <div class="menubg">
-    <div class="left-container">
-    </div>
-    <div class="right-container">
-        <div class="btn-container">
-            <img class="btn" src="../static/images/rankmatch.png" alt="ランクマッチ">
-            <img class="btn" id="friendmatch-btn" src="../static/images/friendmatch.png" alt="フレンドマッチ">
-            <img class="btn" src="../static/images/training.png" alt="トレーニング">
-        </div>  
-    </div>            
-</div>"""
+    html = """<div id="bg" class="start">   
+        <div id="title-container"> 
+            <div class="title">
+                <img id="title-img" src="../static/images/title.png" alt="タイトル">
+            </div>
+            <div class="title-nav">
+                <img id="title-nav-img" src="../static/images/clickanywhere.png" alt="クリックしてください">
+            </div>   
+        </div>       
+    </div> """
     return render_template("index.html", html=html)
 
 if __name__ == "__main__":
