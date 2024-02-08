1b340574bbe4f2a4b28dbe9b81ef01ba4e9a5dde
BrownLunch <gburger125@gmail.com>
2024-02-09 00:47:58 +0900

    fadeinout実装

--- static/js/index.js
+++ static/js/index.js
@@ -16,18 +16,33 @@ function switching_menu(){
     $("body").off("mouseup", switching_menu);
 }
 
+function slide_in(){
+    $(".btn1").removeClass("slide-out");
+    $("#rankmatch-btn").addClass("slide-in1")
+    $("#friendmatch-btn").addClass("slide-in2")
+    $("#training-btn").addClass("slide-in3")
+}
+
 function slide_out(){
+    $(".btn1").removeClass("slide-in1 slide-in2 slide-in3");
     $(".btn1").addClass("slide-out");
 }
 
 $("body").on("mouseup", switching_menu);
 $("#friendmatch-btn").on("mouseup", function(){
     slide_out()
-    $(".friendmatch-container").fadeIn(1000)
-    $(".friendmatch-container").css("display", "flex")
+    $(".friendmatch-container").fadeIn(500)
+    $(".friendmatch-container").css("display", "flex");
 });
-
-
+$(".back-btn").on("mousedown", function(){
+    $(".back-btn").css("height", "70%");  
+})
+$(".back-btn").on("mouseup", function(){
+    $(".back-btn").css("height", "60%");
+})
+$(".back-btn").on("click", function(){
+    $(".friendmatch-container").fadeOut(500, slide_in);
+})
 
 
 
